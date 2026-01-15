3065d0eb8d77d5a58ab990efecb6ecb00e1bf0d0
FabriceGhislain7 <ghislaintebou@gmail.com>
2026-01-15 14:30:07 +0100

    Primo test dei metodi della classe Personaggio

---
+++ tests/test_characters/test_personaggio.py
@@ -0,0 +1,9 @@
+from engine_core.characters.personaggio import Personaggio
+
+def test_attacco_generato():
+    pg = Personaggio(nome="Zoro", classe="Gueriero")
+    danno = pg.attacca()
+    
+    assert isinstance(danno, int)
+    assert danno >= 0
+    assert danno <= pg.attacco_max + 10
